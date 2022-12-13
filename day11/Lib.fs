b648f33aca0e026a29e906722251e1ad1d54b19b
spencerwi <spencerwi@gmail.com>
2022-12-13 11:00:34 -0500

    Day11, part 1. Part 2 is some kind of obscure math thing.

---
+++ day11/Lib.fs
@@ -0,0 +1,160 @@
+module Lib
+
+open System
+open System.Text.RegularExpressions
+open System.Collections.Generic
+
+let split_sequence_on (delimiter : 'a) (sequence : 'a seq) : 'a seq seq =
+    seq {
+        let mutable current = Seq.empty in
+        for item in sequence do
+            if item = delimiter then
+                yield current
+                current <- Seq.empty
+            else
+                current <- Seq.append current (seq { item })
+        yield current
+    }
+
+module Monkeys = begin
+
+    module Operation = begin
+        type Operand = 
+            | Constant of int
+            | Old
+
+        type t =
+            | Add of Operand * Operand
+            | Subtract of Operand * Operand
+            | Multiply of Operand * Operand
+            | Divide of Operand * Operand
+
+        let parse x operator y =
+            let op1 = 
+                match x with
+                | "old" -> Old
+                | _ -> Constant (int x)
+            in
+            let op2 = 
+                match y with
+                | "old" -> Old
+                | _ -> Constant (int y)
+            in
+            match operator with
+            | "+" -> Add (op1, op2)
+            | "-" -> Subtract (op1, op2)
+            | "*" -> Multiply (op1, op2)
+            | "/" -> Divide (op1, op2)
+            | _ -> failwith ("Unrecognized operator: " + operator)
+
+        let eval_operand (old_value : int) = function
+            | Constant x -> x
+            | Old -> old_value
+
+        let eval (operation: t) (old_value : int) =
+            match operation with
+            | Add (op1, op2) -> (eval_operand old_value op1) + (eval_operand old_value op2)
+            | Subtract (op1, op2) -> (eval_operand old_value op1) - (eval_operand old_value op2)
+            | Multiply (op1, op2) -> (eval_operand old_value op1) * (eval_operand old_value op2)
+            | Divide (op1, op2) -> (eval_operand old_value op1) / (eval_operand old_value op2)
+
+    end
+
+    type t = {
+        number : int
+        items : Queue<int>
+        operation : Operation.t
+        divisibility_test : int
+        true_target : int
+        false_target : int
+        mutable inspection_count : uint64
+    }
+
+
+    let parse (input : string seq) : t =
+        let lines = Array.ofSeq input in
+        let number = int <| (lines.[0].Split [|' '|]).[1].Replace(":", "") in
+        let starting_items = [
+            for regex_match in Regex.Matches(lines.[1], "(?<item>\d+)") do
+                let item = regex_match.Groups["item"].Value in
+                yield int item
+        ] in
+        let operation_match = Regex.Match(lines.[2], "new = (?<x>old|\d+) (?<operator>\+|-|\*|/) (?<y>old|\d+)") in
+        let operation = 
+            Operation.parse operation_match.Groups["x"].Value operation_match.Groups["operator"].Value operation_match.Groups["y"].Value in
+        let divisibility_test = 
+            int <| Regex.Match(lines.[3], "divisible by (?<factor>\d+)").Groups["factor"].Value 
+        in
+        let true_target = 
+            int <| Regex.Match(lines.[4], "throw to monkey (?<monkey>\d+)").Groups["monkey"].Value 
+        in
+        let false_target = 
+            int <| Regex.Match(lines.[5], "throw to monkey (?<monkey>\d+)").Groups["monkey"].Value 
+        in
+        {
+            number = number
+            items = new Queue<int>(starting_items)
+            operation = operation
+            divisibility_test = divisibility_test
+            true_target = true_target
+            false_target = false_target
+            inspection_count = 0UL
+        }
+
+    let step (do_division : bool) (monkeys : t array) : t array =
+        let new_monkeys = Array.copy monkeys in
+        let take_turn monkey_number =
+            let monkey = new_monkeys.[monkey_number] in
+            let this_round_inspection_count = uint64 monkey.items.Count in
+            monkey.inspection_count <- monkey.inspection_count + this_round_inspection_count
+            while monkey.items.Count > 0 do
+                let item = monkey.items.Dequeue() in
+                let operation_result = 
+                    item
+                    |> (Operation.eval monkey.operation)
+                in
+                let final_worry_level = 
+                    if do_division then
+                        operation_result / 3
+                    else
+                        operation_result
+                in
+                let target = 
+                    if final_worry_level % (monkey.divisibility_test) = 0 then
+                        monkey.true_target
+                    else
+                        monkey.false_target
+                in
+                new_monkeys.[target].items.Enqueue final_worry_level 
+        in
+        for i = 0 to (monkeys.Length - 1) do 
+            take_turn i
+        done;
+        new_monkeys
+
+    let monkey_business_level (monkeys : t array) : uint64 =
+        monkeys
+        |> Seq.map (fun monkey -> monkey.inspection_count)
+        |> Seq.sortDescending 
+        |> Seq.take 2
+        |> Seq.reduce (*)
+end
+
+module Puzzle = begin
+    let run (do_division : bool) (round_count : int) (input : string seq) : uint64 =
+        let mutable monkeys = 
+            input
+            |> split_sequence_on ""
+            |> Array.ofSeq
+            |> Array.map Monkeys.parse
+        for round = 1 to round_count do
+            monkeys <- Monkeys.step do_division monkeys
+        done;
+        Monkeys.monkey_business_level monkeys
+
+    let part1 (input: string seq) =
+        run true 20 input
+
+    let part2 (input: string seq) =
+        run false 10_000 input
+end
---
+++ day11/Tests.fs
@@ -0,0 +1,100 @@
+module Tests
+open System
+open System.Collections.Generic
+open Lib
+
+open NUnit.Framework
+open FsUnit
+
+let sample_input = [|
+    "Monkey 0:";
+    "  Starting items: 79, 98";
+    "  Operation: new = old * 19";
+    "  Test: divisible by 23";
+    "    If true: throw to monkey 2";
+    "    If false: throw to monkey 3";
+    "";
+    "Monkey 1:";
+    "  Starting items: 54, 65, 75, 74";
+    "  Operation: new = old + 6";
+    "  Test: divisible by 19";
+    "    If true: throw to monkey 2";
+    "    If false: throw to monkey 0";
+    "";
+    "Monkey 2:";
+    "  Starting items: 79, 60, 97";
+    "  Operation: new = old * old";
+    "  Test: divisible by 13";
+    "    If true: throw to monkey 1";
+    "    If false: throw to monkey 3";
+    "";
+    "Monkey 3:";
+    "  Starting items: 74";
+    "  Operation: new = old + 3";
+    "  Test: divisible by 17";
+    "    If true: throw to monkey 0";
+    "    If false: throw to monkey 1"
+|]
+
+[<TestFixture>]
+type ``Tests for Monkeys module`` ()=
+
+    [<Test>]
+    member this.``It parses a monkey correctly`` ()=
+        Monkeys.parse sample_input.[0..5]
+        |> should equal {
+            Monkeys.number = 0
+            Monkeys.items = new Queue<int>([79; 98])
+            Monkeys.operation = Monkeys.Operation.Multiply (Monkeys.Operation.Old, Monkeys.Operation.Constant 19)
+            Monkeys.divisibility_test = 23
+            Monkeys.true_target = 2
+            Monkeys.false_target = 3
+            Monkeys.inspection_count = 0UL
+        }
+
+    [<Test>]
+    member this.``It plays a round with division correctly`` ()=
+        let monkeys = 
+            sample_input
+            |> Lib.split_sequence_on ""
+            |> Array.ofSeq
+            |> Array.map Monkeys.parse
+        in
+        Monkeys.step true monkeys
+        |> Array.map (fun monkey -> monkey.items.ToArray())
+        |> should equal [|
+            [| 20; 23; 27; 26 |];
+            [| 2080; 25; 167; 207; 401; 1046 |];
+            Array.empty;
+            Array.empty
+        |]
+
+        // TODO
+//    [<Test>]
+//    member this.``It plays a round without division correctly`` ()=
+//        let monkeys = 
+//            sample_input
+//            |> Lib.split_sequence_on ""
+//            |> Array.ofSeq
+//            |> Array.map Monkeys.parse
+//        in
+//        Monkeys.step true monkeys
+//        |> Array.map (fun monkey -> monkey.items.ToArray())
+//        |> should equal [|
+//            [| 20; 23; 27; 26 |];
+//            [| 2080; 25; 167; 207; 401; 1046 |];
+//            Array.empty;
+//            Array.empty
+//        |]
+
+[<TestFixture>]
+type ``Tests for solution`` ()=
+    [<Test>]
+    member this.``It should solve part 1`` ()=
+        Puzzle.part1 sample_input
+        |> should equal 10605UL
+
+    [<Test>]
+    member this.``It should solve part 2`` ()=
+        Puzzle.part2 sample_input
+        |> should equal 2713310158UL
