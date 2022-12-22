58d694e5cb63db899ec525e9f8a081c71fb51f72
spencerwi <spencerwi@gmail.com>
2022-12-22 11:23:55 -0500

    Day16: the mechanics work, but the heuristic is wrong

---
+++ day16/Lib.fs
@@ -0,0 +1,210 @@
+module Lib
+
+open System
+open System.Text.RegularExpressions
+
+module Valves = begin
+    type Valve = {
+        name : string
+        flow_rate : int
+        mutable is_open : bool
+        mutable connections : string list
+    } with
+        static member parse (input : string) =
+            let match_groups = 
+                Regex.Match(
+                    input,
+                    "Valve (?<name>[A-Z]+) has flow rate=(?<flow_rate>[0-9]+); (tunnels lead to valves|tunnel leads to valve) (?<connections>([A-Z]{2}(, )*)+)"
+                ).Groups 
+            in
+            let connections = 
+                let connections_str = match_groups["connections"].Value in
+                if connections_str.Contains ","  then
+                    connections_str.Split(",", StringSplitOptions.RemoveEmptyEntries)
+                    |> Seq.map (fun s -> s.Trim())
+                    |> List.ofSeq
+                else
+                    [ connections_str.Trim() ]
+            in
+            {
+                name = match_groups["name"].Value
+                flow_rate = int (match_groups["flow_rate"].Value)
+                is_open = false
+                connections = connections
+            }
+
+        member this.open_valve () =
+            this.is_open <- true
+
+
+    type ValveSystem = {
+        mutable valves : Map<string, Valve>
+        mutable travel_times : Map<(string * string), int>
+    } with
+        member this.Item 
+            with get (valve_name : string) = 
+                this.valves[valve_name]
+            and set (valve_name : string) (valve : Valve) =
+                this.valves <- this.valves.Add(valve_name, valve)
+
+        member this.open_valves =
+            this.valves.Values
+            |> Seq.filter (fun valve -> valve.is_open)
+
+        member this.closed_valves =
+            this.valves.Values
+            |> Seq.filter (fun valve -> not valve.is_open)
+
+        member this.total_flow_rate =
+            this.open_valves
+            |> Seq.map (fun valve -> valve.flow_rate)
+            |> Seq.sum
+
+        member this.travel_time (src : string) (dest : string) =
+            if src = dest then 0
+            else
+                match this.travel_times.TryFind (src, dest) with 
+                | Some t -> t
+                | None ->
+                    let rec move (seen_valves : Set<string>) (current : string) = 
+                        if current = dest then 
+                            Some 0
+                        else
+                            let current_valve = this[current] in
+                            let next_steps = 
+                                current_valve.connections
+                                |> Seq.filter (not << seen_valves.Contains)
+                            in
+                            if Seq.isEmpty next_steps then
+                                None
+                            else
+                                next_steps
+                                |> Seq.map (fun next ->
+                                    move (seen_valves.Add current) next
+                                )
+                                |> Seq.filter Option.isSome // eliminate results that had no path
+                                |> Seq.map Option.get // unwrap those
+                                |> Seq.sort // sort ascending, shortest child distance first
+                                |> Seq.tryHead // try to take that shortest one if it exists
+                                |> Option.map (fun distance -> distance + 1) // and add 1 to it
+                    in 
+                    let travel_time = move Set.empty src in
+                    this.travel_times <- this.travel_times.Add((src, dest), travel_time.Value);
+                    travel_time.Value
+
+        member this.highest_flow_rate_closed_valve =
+            this.valves.Values
+            |> Seq.filter (fun valve -> not valve.is_open)
+            |> Seq.maxBy (fun valve -> valve.flow_rate)
+
+        static member parse (input : string seq) =
+            let valves =
+                input 
+                |> Seq.map Valve.parse
+                |> Seq.map (fun valve -> (valve.name, valve))
+                |> Map.ofSeq
+            in
+            // Initialize travel times with 1-step connections
+            let travel_times =
+                valves.Values
+                |> Seq.collect (fun src ->
+                    seq { 
+                        for dest in src.connections do
+                            yield ((src.name, dest), 1)
+                    }
+                )
+                |> Map.ofSeq
+            in { 
+                valves = valves 
+                travel_times = travel_times
+            }
+
+
+    type State = {
+        valve_system : ValveSystem
+        mutable time_left : int
+        mutable current_location: string
+        mutable total_pressure_released : int
+    } with
+        static member make (valve_system : ValveSystem) =
+            {
+                valve_system = valve_system
+                time_left = 30
+                current_location = "AA"
+                total_pressure_released = 0
+            }
+
+        member this.tick() =
+            printfn "== Minute %d ==" (31 - this.time_left);
+            let valve_status = 
+                if Seq.isEmpty this.valve_system.open_valves then Console.WriteLine "No valves are open."
+                elif Seq.length this.valve_system.open_valves = 1 then 
+                    let open_valve = Seq.exactlyOne this.valve_system.open_valves in
+                    printfn "Valve %s is open, releasing %d pressure." open_valve.name this.valve_system.total_flow_rate
+                else
+                    let open_valve_names = 
+                        this.valve_system.open_valves
+                        |> Seq.map (fun v -> v.name)
+                        |> String.concat ", "
+                    in
+                    printfn "Valves %s are open, releasing %d pressure." open_valve_names this.valve_system.total_flow_rate
+            this.total_pressure_released <- this.total_pressure_released + this.valve_system.total_flow_rate
+            this.time_left <- this.time_left - 1
+
+        member this.open_valve (valve : Valve) =
+            this.tick();
+            printfn "You open valve %s." valve.name
+            valve.is_open <- true
+
+        member this.move_to (dest : Valve) =
+            let travel_time = this.valve_system.travel_time this.current_location dest.name in
+            printfn "Moving to %s" dest.name;
+            for t in 1 .. travel_time do
+                if this.time_left > 0 then
+                    this.tick();
+            done
+            this.current_location <- dest.name
+
+end
+
+module Puzzle = begin
+    open Valves // lol
+
+    let part1 (input: string seq) =
+        let valve_system = ValveSystem.parse input in
+        let state = State.make valve_system in
+        while state.time_left > 0 do
+            // Let's try a strategy of "always go to the closest one that has the highest yield"
+            // TODO: this strategy is wrong. Need to identify a different strategy for picking my next move.
+            let closed_valves_worth_opening =
+                valve_system.closed_valves
+                |> Seq.filter (fun valve -> valve.flow_rate > 0) // 0-pressure valves aren't worth opening.
+            if Seq.isEmpty closed_valves_worth_opening then
+                state.tick();
+            else
+                let (travel_time_used, dest) = 
+                    valve_system.closed_valves
+                    |> Seq.filter (fun valve -> valve.flow_rate > 0) 
+                    |> Seq.map (fun valve -> 
+                        let travel_time = 
+                            valve_system.travel_time state.current_location valve.name
+                        in
+                        travel_time, valve
+                    )
+                    |> Seq.sortWith (fun (cost1, valve1) (cost2, valve2) ->
+                        if compare cost1 cost2 <> 0 then
+                            compare cost1 cost2
+                        else
+                            compare valve2.flow_rate valve1.flow_rate
+                    )
+                    |> Seq.head
+                in
+                state.move_to dest;
+                if state.time_left > 0 then
+                    state.open_valve dest;
+        done;
+        state.total_pressure_released
+
+    let part2 (input: string seq) =
+        "the right answer"
+end
---
+++ day16/Tests.fs
@@ -0,0 +1,78 @@
+module Tests
+open Lib
+
+open NUnit.Framework
+open FsUnit
+
+open type Valves.Valve
+
+let sample_input = [|
+    "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB";
+    "Valve BB has flow rate=13; tunnels lead to valves CC, AA";
+    "Valve CC has flow rate=2; tunnels lead to valves DD, BB";
+    "Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE";
+    "Valve EE has flow rate=3; tunnels lead to valves FF, DD";
+    "Valve FF has flow rate=0; tunnels lead to valves EE, GG";
+    "Valve GG has flow rate=0; tunnels lead to valves FF, HH";
+    "Valve HH has flow rate=22; tunnel leads to valve GG";
+    "Valve II has flow rate=0; tunnels lead to valves AA, JJ";
+    "Valve JJ has flow rate=21; tunnel leads to valve II"
+|]
+
+[<TestFixture>]
+type ``Tests for Valves`` ()= 
+
+    [<Test>]
+    member this.``It should parse an individual valve correctly`` ()=
+        Valves.Valve.parse sample_input[1]
+        |> should equal {
+            name = "BB"
+            flow_rate = 13
+            is_open = false
+            connections = [ "CC"; "AA" ]
+        }
+
+    [<Test>]
+    member this.``It should parse a ValveSystem correctly`` ()=
+        let valve_system = Valves.ValveSystem.parse sample_input in
+        valve_system.valves
+        |> should equal <| Map.ofSeq (seq {
+            ("AA", { name = "AA"; flow_rate = 0; connections = ["DD"; "II"; "BB"]; is_open = false });
+            ("BB", { name = "BB"; flow_rate = 13; connections = ["CC"; "AA"]; is_open = false });
+            ("CC", { name = "CC"; flow_rate = 2; connections = ["DD"; "BB"]; is_open = false });
+            ("DD", { name = "DD"; flow_rate = 20; connections = ["CC"; "AA"; "EE"]; is_open = false });
+            ("EE", { name = "EE"; flow_rate = 3; connections = ["FF"; "DD"]; is_open = false });
+            ("FF", { name = "FF"; flow_rate = 0; connections = ["EE"; "GG"]; is_open = false });
+            ("GG", { name = "GG"; flow_rate = 0; connections = ["FF"; "HH"]; is_open = false });
+            ("HH", { name = "HH"; flow_rate = 22; connections = ["GG"]; is_open = false });
+            ("II", { name = "II"; flow_rate = 0; connections = ["AA"; "JJ"]; is_open = false });
+            ("JJ", { name = "JJ"; flow_rate = 21; connections = ["II"]; is_open = false });
+        })
+
+    [<Test>]
+    member this.``It should figure out travel times correctly`` ()=
+        let valve_system = Valves.ValveSystem.parse sample_input in
+
+        valve_system.travel_time "AA" "DD"
+        |> should equal 1
+
+        valve_system.travel_time "AA" "AA"
+        |> should equal 0
+
+        valve_system.travel_time "AA" "CC"
+        |> should equal 2
+
+        valve_system.travel_time "AA" "GG"
+        |> should equal 4
+
+[<TestFixture>]
+type ``Tests for solution`` ()=
+    [<Test>]
+    member this.``It should solve part 1`` ()=
+        Puzzle.part1 sample_input
+        |> should equal 1651
+
+    [<Test>]
+    member this.``It should solve part 2`` ()=
+        Puzzle.part2 sample_input
+        |> should equal "the right answer"
